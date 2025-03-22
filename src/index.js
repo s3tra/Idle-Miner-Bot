ff7dce62358ec59ccee8df7bdfeff12cfff56556
s3tra <dropemail01@pm.me>
2025-03-22 04:24:38 +0000

    Update todo list

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
   TODO:
   
   [LOW] Convert progress bars to custom emojis.
-  [LOW] Add other leaderboard options, such as company income.
   [MED] Add a character limit to company names.
   
   [LOW] Make a create argument for getData() - storelite2.
