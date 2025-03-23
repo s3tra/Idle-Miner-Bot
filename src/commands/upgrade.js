e179c79ff63ef152fbdd5d3996d49cfbc3ef9e0f
s3tra <dropemail01@pm.me>
2025-03-23 02:04:05 +0000

    Custom emojis for progress bars

--- src/commands/upgrade.js
+++ src/commands/upgrade.js
@@ -16,10 +16,51 @@ const data = new SlashCommandBuilder()
   .setDescription('Level up your company.');
 
 const generateProgressBar = (cur, max) => {
+  const emojis = {
+    roundStartEmpty: '<:EmptyLXPBar:1353004698025590856>',
+    straightMiddleEmpty: '<:EmptyMXPBar:1353004432320892968>',
+    roundEndEmpty: '<:EmptyRXPBar:1353004697090527313>',
+
+    roundStartFull: '<:FullLXPBar:1353013015943057520>',
+    straightMiddleFull: '<:FullMXPBar:1353014241916878920>',
+    roundEndFull: '<:FullRXPBar:1353013018912624700>',
+  };
+
   let bar = '';
 
-  for (let i = 0; i < cur; i++) bar += '🟦';
-  for (let i = cur; i < max; i++) bar += '⬜';
+  if (cur === 0) {
+    bar =
+      emojis.roundStartEmpty +
+      emojis.straightMiddleEmpty +
+      emojis.straightMiddleEmpty +
+      emojis.straightMiddleEmpty +
+      emojis.roundEndEmpty;
+  } else if (cur === max) {
+    bar =
+      emojis.roundStartFull +
+      emojis.straightMiddleFull +
+      emojis.straightMiddleFull +
+      emojis.straightMiddleFull +
+      emojis.roundEndFull;
+  } else {
+    for (let i = 0; i < cur; i++) {
+      if (i === 0) {
+        bar += emojis.roundStartFull;
+      } else if (i === 4) {
+        bar += emojis.roundEndFull;
+      } else {
+        bar += emojis.straightMiddleFull;
+      }
+    }
+
+    for (let i = cur; i < max; i++) {
+      if (i === 4) {
+        bar += emojis.roundEndEmpty;
+      } else {
+        bar += emojis.straightMiddleEmpty;
+      }
+    }
+  }
 
   return bar;
 };
@@ -74,18 +115,30 @@ const execute = async (interaction, userData) => {
     );
 
     if (!company.upgrades[i.customId]) company.upgrades[i.customId] = 0;
-    
-    if (company.upgrades[i.customId] === config.base_upgrades[i.customId].maxUpgrade)
+
+    if (
+      company.upgrades[i.customId] ===
+      config.base_upgrades[i.customId].maxUpgrade
+    )
       return i.reply({
-        embeds: [createErrorEmbed('This item is already at the maximum upgrade level.')],
+        embeds: [
+          createErrorEmbed(
+            'This item is already at the maximum upgrade level.'
+          ),
+        ],
         flags: MessageFlags.Ephemeral,
       });
 
     const upgradeCost =
-      config.base_upgrades[i.customId].basePrice * (company.upgrades[i.customId] + 1);
+      config.base_upgrades[i.customId].basePrice *
+      (company.upgrades[i.customId] + 1);
     if (company.bank < upgradeCost)
       return i.reply({
-        embeds: [createErrorEmbed(`You need $${upgradeCost.toLocaleString()} to purchase this.`)],
+        embeds: [
+          createErrorEmbed(
+            `You need $${upgradeCost.toLocaleString()} to purchase this.`
+          ),
+        ],
         flags: MessageFlags.Ephemeral,
       });
 
